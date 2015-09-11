0e91945baa9a1c5e92ef5e218808fb64716b742f
Alexander Kentner <akentner@lexsign.de>
2015-09-11 15:09:26 +0200

    working sample

--- src/app.js
+++ src/app.js
@@ -4,42 +4,45 @@ var tcp = require('net');
 var fritzConnection;
 var mqttConnection;
 
-mqttConnection = mqtt.connect('tcp://localhost:1883', {will: {topic: "fritz/connect", payload: "0", qos: 1}});
+mqttConnection = mqtt.connect('tcp://localhost:1883', {
+    protocolId: 'MQIsdp',
+    protocolVersion: 3, 
+    will: {topic: "fritz/connect", payload: "0", qos: 1}
+});
 
 mqttConnection.on('connect', function () {
 
-    fritzConnection = tcp.connect('80', 'localhost');
+    fritzConnection = tcp.connect('1012', '192.168.178.1');
 
     fritzConnection.on('data', function(data) {
-        console.log('callmonitor:','' + data);
 
-        mqttConnection.publish('fritz/connect', '1');
+        
 
+
+        mqttConnection.publish('fritz/callmonitor/', data);
     }).on('connect', function() {
-        mqttConnection.publish('fritz/connect', '1');
+        console.log('callmonitor connect');
+        mqttConnection.publish('fritz/callmonitor/connect', '1');
     }).on('end', function() {
-        console.log('Disconnected');
+        mqttConnection.publish('fritz/callmonitor/connect', '0');
     });
-
 
 });
 
 mqttConnection.on('reconnect', function () {
-    mqttConnection.publish('fritz/connect', '1');
+    console.log('mqtt reconnect');
 });
 
 mqttConnection.on('close', function () {
-    console.log('close');
+    console.log('mqtt close');
 });
 
 mqttConnection.on('error', function (error) {
-    console.log(error);
+    console.log('mqtt error: ', error);
 });
 
 mqttConnection.on('message', function (topic, message) {
-    // message is Buffer
-    console.log(message.toString());
-    //client.end();
+    console.log('mqtt message:', topic, message.toString());
 });
 
 
